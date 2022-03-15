9e1573cb31988c5c3e35645f57bc956f661a36c4
“ColbyCorder” <colbycorder@gmail.com>
2022-03-15 11:59:42 -0400

    Created version 1.0.1

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function myFunction(){
     alert("I'm sorry, but this website is not done yet. Come back soon and you'll see what a cool website this will be. Thank you!")
+};
+
+function aboutMe(){
+    alert("Aww, I'm flattered you want to learn about me, but I'll talk more about me at a later time ;3")
+};
+
+function project(){
+    alert("Right now I'm working on this, but you'll see my other work soon.")
+};
+
+function resume(){
+    alert("I don't have a funny transition for this one so let's talk about helium. Did you know that all of earths helium will be gone in the next few years? Kind of scary to think about. Now you'll think twice about using it for nonsense")
+};
+
+function contact(){
+    alert("Sorry I would give you my number, but I have a loving girlfriend that would kill me if I gave it to you. I'll give you hers if you want to talk about it")
 }

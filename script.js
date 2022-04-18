7d4a02ed0e17517a03f5d41bfd52a1c1595ce778
“ColbyCorder” <colbycorder@gmail.com>
2022-04-17 22:21:04 -0400

    Added Responsive Navbar/Tabs

--- script.js
+++ script.js
@@ -46,3 +46,29 @@ function randomNum(){
             alert(`AMAZING!!! YOU ROLLED A "${ranNum}" YOU ARE GONNA HAVE A AWESOME DAY!!!`);
         }
       };
+
+ // NAVBAR
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+// TABS
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
